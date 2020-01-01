bfe58a8bbe521557188e60d81c1922d4c7b1b489
Anthony Liu <anthonyliuhao@icloud.com>
2019-12-31 19:11:06 -0800

    Start a Constants file

---
+++ Todoey/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Todoey
+//
+//  Created by Anthony Liu on 2019/12/31.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct K {
+    static let cellIdentifier = "ToDoItemCell"
+}
